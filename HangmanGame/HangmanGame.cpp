8ac333c3fd155d30b10179503534de87765f9a3d
Camilo Barreto <csbarreto11@gmail.com>
2024-01-03 14:48:09 +0000

    Initial Commit

--- HangmanGame/HangmanGame.cpp
+++ HangmanGame/HangmanGame.cpp
@@ -1,20 +1,35 @@
-// HangmanGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*
+            Game GUI Visualisation
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    +---------------------------------+
+    |             HANG MAN            |
+    +---------------------------------+
+    |               |                 |
+    |               |                 |
+    |               O                 |
+    |              /|\                |
+    |               |                 |
+    |              / \                |
+    |         +----------+            |
+    |         |          |            |
+    +---------------------------------+
+    |        Available letters        |
+    +---------------------------------+
+    |     A B C D E F G H I J K L M   |
+    |     N O P Q R S T U V W X Y Z   |
+    +---------------------------------+
+    |         Guess the word          |
+    +---------------------------------+
+    | _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |
+    +---------------------------------+
+    >
+
+*/
