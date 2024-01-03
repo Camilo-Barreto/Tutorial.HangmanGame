a3f7b64dd094a0dc308439667f1146ec89e26cb8
Camilo Barreto <csbarreto11@gmail.com>
2024-01-03 15:22:54 +0000

    Print header function completed

--- HangmanGame/HangmanGame.cpp
+++ HangmanGame/HangmanGame.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
+void PrintHeader(std::string message, bool printTop = true, bool printBottom = true)
+{
+    // The following will print the top of the header
+    std::cout << "+---------------------------------+" << std::endl;
+    std::cout << "|"; 
+
+    // Use a boolean to keep alternately add spaces around the message to center it
+    bool front = true;
+
+    // iterate back and forth to add the spaces to center the message
+    // Start counter i at the message length to stay within the 33 char limit
+    for (int i = message.length(); i < 33; i++)
+    {
+        // first add a space to the front
+        if (front)
+        {
+            message = " " + message;
+        }
+        // else add space to the end
+        else
+        {
+            message = message + " ";
+        }
+        // to alternate back and forth, perform the not operation on the bool
+        front = !front;
+
+        //// DEBUGGING - visualise the spaces added back and forth (comment later)
+        //std::cout << message << std::endl;
+    }
+
+    // Print the actual message 
+    std::cout << message; 
+
+    // Now close the header box as per the GUI visualisatuion below
+    if (printBottom)
+    {
+        std::cout << "|" << std::endl; // use endl to move to the next line 
+        std::cout << "+---------------------------------+" << std::endl;
+
+    }
+ }
+
 int main()
 {
-    std::cout << "";
+    PrintHeader("Hang Man");
+
+    return 0;
 
 }
 
